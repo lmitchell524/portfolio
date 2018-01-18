83ce7f3c79378d7c6db010a3db63d6d1c4013af0
lmitchell524 <lmitchell524@gmail.com>
2018-01-18 13:00:05 -0800

    setup php mailer file for portfolio form

---
+++ server/php_mailer/email_config_default.php
@@ -0,0 +1,7 @@
+<?php
+define('SENDER_EMAIL_USER','<YOUR EMAIL>');
+define('EMAIL_PASS','<YOUR PASSWORD>');
+define('EMAIL_USERNAME','<YOUR USERNAME>');
+define('EMAIL_USERNAME','<YOUR USERNAME>');
+define('RECEIVER_USER','<YOUR NAME>');
+?>
