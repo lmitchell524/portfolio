b4dacf0e2a6d4b9c43a968db338b15bb9c40e5b2
lmitchell524 <lmitchell524@gmail.com>
2018-01-21 20:50:05 -0800

    resolved merge conflick

---
+++ server/php_mailer/email_config.php
@@ -0,0 +1,7 @@
+<?php
+define('SENDER_EMAIL_USER','lmitchelldev@gmail.com');
+define('EMAIL_PASS','devServer524');
+define('EMAIL_USERNAME','Portfolio Form');
+define('RECEIVER_EMAIL_USER','lmitchell524@gmail.com');
+define('RECEIVER_USER','Lori Mitchell');
+?>
